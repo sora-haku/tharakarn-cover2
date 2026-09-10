d74becfa0a1fbc093d4574853111aa7d746a78bc
sora-haku <hakumetsu.sora@gmail.com>
2026-09-10 15:56:48 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('tharakarn-v1').then(cache => cache.addAll(['./','./index.html','./manifest.json'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
